5124d81891bb5937cfd16c37cbf42bce4c51610d
edm20627 <eng00627@gmail.com>
2020-12-28 04:50:55 +0900

    add routing template

--- main.go
+++ main.go
@@ -2,23 +2,30 @@ package main
 
 import (
 	"net/http"
-	"fmt"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	files := http.FileServer(http.Dir("/public"))
+	files := http.FileServer(http.Dir(config.Static))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
 	mux.HandleFunc("/", index)
+
+	mux.HandleFunc("/err", err)
+
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAcount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThead)
+	mux.HandleFunc("/thread/read", readThead)
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
 		Handler: mux,
 	}
 	server.ListenAndServe()
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Herro, %s!", r.URL.Path[1:])
 }
---
+++ util.go
@@ -0,0 +1,29 @@
+package data
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/edm20627/go_chat/data"
+)
+
+func session(w http.ResponseWriter, r *http.Request) (sess data.Session, err error) {
+	cookie, err := r.Cookie("_cookie")
+	if err == nil {
+		sess = data.Session{Uuid: cookie.Value}
+		if ok, _ := sess.Check(); !ok {
+			err = errors.New("Invalid session")
+		}
+	}
+	return
+}
+
+func generateHTML(w http.ResponsWriter, data interface{}, filenames ...string) {
+	var files []stirng
+	for _, file := range filenames {
+		files = append(files, fmt.Fprintf("template/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFile(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
