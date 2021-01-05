0f43eef660256710ac0f6adb6ca9cdc0a8016cf5
edm20627 <eng00627@gmail.com>
2021-01-06 06:16:53 +0900

    add authenticate function

--- data/user.go
+++ data/user.go
@@ -2,10 +2,36 @@ package data
 
 import "time"
 
+type User struct {
+	Id        int
+	Uuid      string
+	Name      string
+	Email     string
+	Password  string
+	CreatedAt time.Time
+}
+
 type Session struct {
-	Id       int
-	Uuid     string
-	Email    string
-	UserId   int
-	CreateAt time.Time
+	Id        int
+	Uuid      string
+	Email     string
+	UserId    int
+	CreatedAt time.Time
+}
+
+func (user *User) CreateSession() (session Session, err error) {
+	statement := "insert into sessions (uuid, email, user_id, created_at) values ($1, $2, $3, $4) returnning id, uuid, email, user_id, created_at"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(createUUID(), user.Email, user.Id, time.Now()).Scan(&session.Id, &session.Uuid, &session.Email, &session.UserId, &session.CreatedAt)
+	return
+}
+
+func UserByEmail(email string) (user User, err error) {
+	user = User{}
+	err = Db.QueryRow("SELECT id, uuid, name, name, email, password, created_at FROM users WHERE email = $1", email).Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.Password, &user.CreatedAt)
+	return
 }
