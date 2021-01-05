192095fd4e47b60b65ccdc9dc726fd03389700e2
edm20627 <eng00627@gmail.com>
2021-01-06 04:43:48 +0900

    add configuration and logger

--- util.go
+++ util.go
@@ -1,12 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/edm20627/go_chat/data"
 )
+
+type Configuration struct {
+	Address      string
+	ReadTimeout  int64
+	WriteTimeout int64
+	Static       string
+}
+
+var config Configuration
+
+var logger *log.Logger
+
+func int() {
+	loadConfig()
+	file, err := os.OpenFile("development.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file", err)
+	}
+	logger = log.New(file, "INFO ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func loadConfig() {
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatalln("Cannot open config file", err)
+	}
+	decoder := json.NewDecoder(file)
+	config = Configuration{}
+	err = decoder.Decode(config) // 確認
+	if err != nil {
+		log.Fatalln("Cannot get configuration from file", err)
+	}
+}
 
 func session(w http.ResponseWriter, r *http.Request) (sess data.Session, err error) {
 	cookie, err := r.Cookie("_cookie")
@@ -20,11 +57,11 @@ func session(w http.ResponseWriter, r *http.Request) (sess data.Session, err err
 }
 
 func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
-	var files []stirng
+	var files []string
 	for _, file := range filenames {
-		files = append(files, fmt.Fprintf("template/%s.html", file))
+		files = append(files, fmt.Sprintf("template/%s.html", file))
 	}
 
-	templates := template.Must(template.ParseFile(files...))
+	templates := template.Must(template.ParseFiles(files...))
 	templates.ExecuteTemplate(w, "layout", data)
 }
