75a8b772a64b8f1f79b678a709e99397cc4b3031
edm20627 <eng00627@gmail.com>
2021-01-07 03:08:36 +0900

    add error handling

--- route_main.go
+++ route_main.go
@@ -8,7 +8,9 @@ import (
 
 func index(w http.ResponseWriter, r *http.Request) {
 	threads, err := data.Threads()
-	if err == nil {
+	if err != nil {
+		error_message(w, r, "Cannot get threads")
+	} else {
 		_, err := session(w, r)
 		if err != nil {
 			generateHTML(w, threads, "layout", "public.navbar", "index")
