cbb29d32d5e54d597d00201ed4acca8d22708c95
edm20627 <eng00627@gmail.com>
2021-01-12 03:58:09 +0900

    create error page

--- route_main.go
+++ route_main.go
@@ -6,6 +6,16 @@ import (
 	"github.com/edm20627/go_chat/data"
 )
 
+func err(w http.ResponseWriter, r *http.Request) {
+	vals := r.URL.Query()
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, vals.Get("msg"), "layout", "public.navbar", "error")
+	} else {
+		generateHTML(w, vals.Get("msg"), "layout", "public.navbar", "error")
+	}
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	threads, err := data.Threads()
 	if err != nil {
