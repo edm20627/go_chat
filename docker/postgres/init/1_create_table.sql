b7db7f85681935010793d9faf99425612a5c14fb
edm20627 <eng00627@gmail.com>
2021-01-15 03:25:04 +0900

    fix sql file

--- docker/postgres/init/1_create_table.sql
+++ docker/postgres/init/1_create_table.sql
@@ -5,7 +5,7 @@ drop table if exists users;
 
 create table users (
   id          serial primary key,
-  uudi        varchar(64) not null unique,
+  uuid        varchar(64) not null unique,
   name        varchar(255),
   email       varchar(255) not null unique,
   password    varchar(255) not null,
